81136629a251363428226735ba21c2887b016481
carlosjuniormp <carlosjuniormp@gmail.com>
2025-04-19 10:47:11 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// Configuração simplificada para Next.js no Vercel
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
